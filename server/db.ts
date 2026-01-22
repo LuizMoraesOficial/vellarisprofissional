0ec38ccde97ec52773842e8ce105509bc198d473
LuizMoraesOficial <luizmoraesofc@gmail.com>
2026-01-22 01:22:36 -0300

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -1,9 +1,11 @@
-import { Pool, neonConfig } from "@neondatabase/serverless";
-import { drizzle } from "drizzle-orm/neon-serverless";
-import ws from "ws";
+import { Pool } from "pg";
+import { drizzle } from "drizzle-orm/node-postgres";
 import * as schema from "@shared/schema";
 
-neonConfig.webSocketConstructor = ws;
+// Isso garante que ele use uma conexão TCP padrão, não WebSockets
+const pool = new Pool({ 
+  connectionString: process.env.DATABASE_URL 
+});
 
-const pool = new Pool({ connectionString: process.env.DATABASE_URL! });
 export const db = drizzle(pool, { schema });
+
